96edc050bca8124c70c348fe5e072c3afdcdc9fd
Silumesii Maboshe <silumesii@thoughtbot.com>
2022-06-21 20:12:48 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/sign_in", to: "sessions#new"
+  post "/sign_in", to: "sessions#create"
+  delete "/sign_out", to: "sessions#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:show]
+  resources :rooms, only: [:show]
+
+  root "rooms#index"
 end
