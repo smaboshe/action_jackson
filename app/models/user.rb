13fc7b2a0ef27370d2de555743b0ba52e85c57df
Silumesii Maboshe <silumesii@thoughtbot.com>
2022-06-21 20:31:53 +0200

    Add turbo streams for rooms and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true
 
   scope :all_except, ->(user) { where.not(id: user) }
+
+  after_create_commit { broadcast_append_to :users }
 end
