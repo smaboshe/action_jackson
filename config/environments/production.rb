0bded1e1e80495884a907985ba035c143f83abe1
Silumesii Maboshe <silumesii@thoughtbot.com>
2022-06-21 20:15:07 +0200

    Apply linting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  config.logformatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
@@ -84,7 +84,7 @@ Rails.application.configure do
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger = ActiveSupport::Logger.new($stdout)
-    logger.formatter = config.log_formatter
+    logger.formatter = config.logformatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
